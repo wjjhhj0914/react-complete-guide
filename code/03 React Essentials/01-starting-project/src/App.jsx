b19cda0faa9471a571339ca1808fea7dfbda37c4
wjjhhj0914 <wjjhhj0914@gmail.com>
2025-11-17 11:20:12 +0900

    learn: 컴포넌트 분리

--- code/03 React Essentials/01-starting-project/src/App.jsx
+++ code/03 React Essentials/01-starting-project/src/App.jsx
@@ -1,16 +1,24 @@
+import { CORE_CONCEPTS } from './data';
+import Header from './components/Header.jsx';
+import CoreConcept from './components/CoreConcept.jsx';
+
 function App() {
   return (
     <div>
-      <header>
-        <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
-        <h1>React Essentials</h1>
-        <p>
-          Fundamental React concepts you will need for almost any app you are
-          going to build!
-        </p>
-      </header>
+      <Header />
       <main>
-        <h2>Time to get started!</h2>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcept
+              image={CORE_CONCEPTS[0].image}
+              title={CORE_CONCEPTS[0].title}
+              description={CORE_CONCEPTS[0].description}
+            />
+            <CoreConcept {...CORE_CONCEPTS[1]} />
+            <CoreConcept {...CORE_CONCEPTS[2]} />
+          </ul>
+        </section>
       </main>
     </div>
   );
