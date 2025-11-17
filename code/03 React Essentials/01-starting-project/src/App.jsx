38c96201cbde5c840cad9f03f0f69a089f19f129
wjjhhj0914 <wjjhhj0914@gmail.com>
2025-11-17 14:34:47 +0900

    learn: State 관리 & Hooks 사용법

--- code/03 React Essentials/01-starting-project/src/App.jsx
+++ code/03 React Essentials/01-starting-project/src/App.jsx
@@ -2,10 +2,13 @@ import { CORE_CONCEPTS } from './data';
 import Header from './components/Header/Header.jsx';
 import CoreConcept from './components/CoreConcept/CoreConcept.jsx';
 import TabButton from './components/TabButton.jsx';
+import { useState } from 'react';
 
 function App() {
+  const [selectedTopic, setSelectedTopic] = useState('Please click a button');
   function handleSelect(selectedButton) {
-    console.log(selectedButton);
+    setSelectedTopic(selectedButton);
+    // console.log(selectedTopic);
   }
 
   return (
@@ -34,6 +37,7 @@ function App() {
             <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
           </menu>
         </section>
+        {selectedTopic}
       </main>
     </div>
   );
