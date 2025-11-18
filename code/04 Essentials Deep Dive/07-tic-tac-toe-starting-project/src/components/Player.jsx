35d796e359adbc37ee54aca5db74625a344cd616
wjjhhj0914 <wjjhhj0914@gmail.com>
2025-11-18 14:55:23 +0900

    learn: (개념 복습) State(상태) 활용법

--- code/04 Essentials Deep Dive/07-tic-tac-toe-starting-project/src/components/Player.jsx
+++ code/04 Essentials Deep Dive/07-tic-tac-toe-starting-project/src/components/Player.jsx
@@ -1,8 +1,26 @@
+import { useState } from 'react';
+
 export default function Player({ name, symbol }) {
+  const [isEditing, setIsEditing] = useState(false);
+
+  function handleEditClick() {
+    // re-execute the Player function again
+    setIsEditing(true);
+  }
+
+  let playerName = <span className="player-name">{name}</span>;
+
+  if (isEditing) {
+    playerName = <input type="text" required />;
+  }
+
   return (
     <li>
-      <span className="player-name">{name}</span>
+      {/* My Solution */}
+      {/* {!isEditing ? <span className="player-name">{name}</span> : <input />} */}
+      {playerName}
       <span className="player-symbol">{symbol}</span>
+      <button onClick={handleEditClick}>Edit</button>
     </li>
   );
 }
