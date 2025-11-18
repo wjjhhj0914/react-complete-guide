6f5743ada621c21a18530079dbd414dbf2551959
wjjhhj0914 <wjjhhj0914@gmail.com>
2025-11-18 10:10:20 +0900

    learn: JSX를 꼭 사용하지 않아도 되는 이유
    - React.createElement

--- code/03 React Essentials/01-starting-project/src/index.jsx
+++ code/03 React Essentials/01-starting-project/src/index.jsx
@@ -1,7 +1,9 @@
-import ReactDOM from "react-dom/client";
+import ReactDOM from 'react-dom/client';
 
-import App from "./App.jsx";
-import "./index.css";
+import App from './App.jsx';
+import './index.css';
+import React from 'react';
 
-const entryPoint = document.getElementById("root");
-ReactDOM.createRoot(entryPoint).render(<App />);
+const entryPoint = document.getElementById('root');
+// ReactDOM.createRoot(entryPoint).render(<App />);
+ReactDOM.createRoot(entryPoint).render(React.createElement(App));
