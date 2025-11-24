1fa96eaf259cc2e1efaee050e231502dd0652b80
wjjhhj0914 <wjjhhj0914@gmail.com>
2025-11-24 10:35:13 +0900

    learn: State 관리 관소화 및 불필요한 State 분별

--- code/04 Essentials Deep Dive/07-tic-tac-toe-starting-project/src/App.jsx
+++ code/04 Essentials Deep Dive/07-tic-tac-toe-starting-project/src/App.jsx
@@ -3,18 +3,26 @@ import Log from './components/Log';
 import { useState } from 'react';
 import GameBoard from './components/GameBoard';
 
+function deriveActivePlayer(gameTurns) {
+  let currentPlayer = 'X';
+
+  if (gameTurns.length > 0 && gameTurns[0].player === 'X') {
+    currentPlayer = 'O';
+  }
+
+  return currentPlayer;
+}
+
 function App() {
   const [gameTurns, setGameTurns] = useState([]);
-  const [activePlayer, setActivePlayer] = useState('X');
+  // const [activePlayer, setActivePlayer] = useState('X');
+
+  const activePlayer = deriveActivePlayer(gameTurns);
 
   function handleSelectSquare(rowIndex, colIndex) {
-    setActivePlayer(currActivePlayer => (currActivePlayer === 'X' ? 'O' : 'X'));
+    // setActivePlayer(currActivePlayer => (currActivePlayer === 'X' ? 'O' : 'X'));
     setGameTurns(prevTurns => {
-      let currentPlayer = 'X';
-
-      if (prevTurns.length > 0 && prevTurns[0].player === 'X') {
-        currentPlayer = 'O';
-      }
+      const currentPlayer = deriveActivePlayer(prevTurns);
 
       const updatedTurns = [
         { square: { row: rowIndex, col: colIndex }, player: activePlayer },
