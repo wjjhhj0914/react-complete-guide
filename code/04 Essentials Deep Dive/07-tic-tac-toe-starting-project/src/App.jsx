f3359a9019a0a8d32d4789b9e0a9c1667dece7bd
wjjhhj0914 <wjjhhj0914@gmail.com>
2025-11-24 10:20:17 +0900

    learn: 컴포넌트 간의 State 공유

--- code/04 Essentials Deep Dive/07-tic-tac-toe-starting-project/src/App.jsx
+++ code/04 Essentials Deep Dive/07-tic-tac-toe-starting-project/src/App.jsx
@@ -34,7 +34,7 @@ function App() {
         </ol>
         <GameBoard onSelectSquare={handleSelectSquare} turns={gameTurns} />
       </div>
-      <Log />
+      <Log turns={gameTurns} />
     </main>
   );
 }
