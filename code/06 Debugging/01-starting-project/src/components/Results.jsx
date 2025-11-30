6bfec7b81226fba18b039017be260c4d45fbf316
wjjhhj0914 <wjjhhj0914@gmail.com>
2025-12-01 00:00:14 +0900

    learn: 리액트 오류 메시지 이해하기

--- code/06 Debugging/01-starting-project/src/components/Results.jsx
+++ code/06 Debugging/01-starting-project/src/components/Results.jsx
@@ -1,9 +1,13 @@
 import { calculateInvestmentResults, formatter } from '../util/investment.js';
 
-
 export default function Results({ input }) {
   const results = [];
   calculateInvestmentResults(input, results);
+
+  if (results.length === 0) {
+    return <p className="center">Invalid input data provided.</p>;
+  }
+
   const initialInvestment =
     results[0].valueEndOfYear -
     results[0].interest -
@@ -21,7 +25,7 @@ export default function Results({ input }) {
         </tr>
       </thead>
       <tbody>
-        {results.map((yearData) => {
+        {results.map(yearData => {
           const totalInterest =
             yearData.valueEndOfYear -
             yearData.annualInvestment * yearData.year -
