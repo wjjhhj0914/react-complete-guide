24d8e10b181dfe5781f73208501234cb1f5651f2
wjjhhj0914 <wjjhhj0914@gmail.com>
2025-12-11 10:47:38 +0900

    learn: 선택 가능한 프로젝트 구현 및 프로젝트 정보 보기

--- code/09 Practice Project - Project Management/01-starting-project/src/App.jsx
+++ code/09 Practice Project - Project Management/01-starting-project/src/App.jsx
@@ -3,12 +3,22 @@ import { useState } from 'react';
 import NewProject from './components/NewProject.jsx';
 import NoProjectSelected from './components/NoProjectSelected.jsx';
 import ProjectsSidebar from './components/ProjectsSidebar.jsx';
+import SelectedProject from './components/SelectedProject.jsx';
 
 function App() {
   const [projectsState, setProjectsState] = useState({
     selectedProjectId: undefined,
     projects: [],
   });
+
+  function handleSelectProject(id) {
+    setProjectsState(prevState => {
+      return {
+        ...prevState,
+        selectedProjectId: id,
+      };
+    });
+  }
 
   function handleStartAddProject() {
     setProjectsState(prevState => {
@@ -43,7 +53,17 @@ function App() {
     });
   }
 
-  let content;
+  // 배열의 한 ID 요소를 찾는 데 사용
+  // find는 함수를 인수로 취하고,
+  // 이 배열의 모든 요소에 실행된 함수다/
+  // 우리가 원하는 요소가 나왔을 때 함수는 TRUE가 된다.
+  // 그럼 find는 그 요소를 반환하게 되고
+  // 찾은 요소가 SelectedProject에 저장된다.
+  const selectedProject = projectsState.projects.find(
+    project => project.id === projectsState.selectedProjectId
+  );
+
+  let content = <SelectedProject project={selectedProject} />;
 
   if (projectsState.selectedProjectId === null) {
     content = (
@@ -58,6 +78,7 @@ function App() {
       <ProjectsSidebar
         onStartAddProject={handleStartAddProject}
         projects={projectsState.projects}
+        onSelectProject={handleSelectProject}
       />
       {content}
     </main>
