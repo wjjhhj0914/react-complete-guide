44aca867879997148a24a3bd1561b9080f8f5429
wjjhhj0914 <wjjhhj0914@gmail.com>
2025-12-12 13:45:42 +0900

    learn: Task 관리 & Prop Drilling 이해하기

--- code/09 Practice Project - Project Management/01-starting-project/src/App.jsx
+++ code/09 Practice Project - Project Management/01-starting-project/src/App.jsx
@@ -9,7 +9,25 @@ function App() {
   const [projectsState, setProjectsState] = useState({
     selectedProjectId: undefined,
     projects: [],
+    tasks: [],
   });
+
+  function handleAddTask(text) {
+    setProjectsState(prevState => {
+      const taskId = Math.random();
+      const newTask = {
+        text: text,
+        projectId: prevState.selectedProjectId,
+        id: taskId,
+      };
+      return {
+        ...prevState,
+        tasks: [newTask, ...prevState.tasks],
+      };
+    });
+  }
+
+  function handleDeleteTask() {}
 
   function handleSelectProject(id) {
     setProjectsState(prevState => {
@@ -82,7 +100,13 @@ function App() {
   );
 
   let content = (
-    <SelectedProject project={selectedProject} onDelete={handleDeleteProject} />
+    <SelectedProject
+      project={selectedProject}
+      onDelete={handleDeleteProject}
+      onAddTask={handleAddTask}
+      onDeleteTask={handleDeleteTask}
+      tasks={projectsState.tasks}
+    />
   );
 
   if (projectsState.selectedProjectId === null) {
--- code/09 Practice Project - Project Management/01-starting-project/src/components/NewTask.jsx
+++ code/09 Practice Project - Project Management/01-starting-project/src/components/NewTask.jsx
@@ -1,8 +1,33 @@
-export default function NewTask() {
+import { useState } from 'react';
+
+export default function NewTask({ onAdd }) {
+  const [enteredTask, setEnteredTask] = useState();
+
+  function handleChange(event) {
+    setEnteredTask(event.target.value);
+  }
+
+  function handleClick() {
+    // 1. 먼저 입력된 값을 App 컴포넌트로 보내기
+    // 2. 이 입력란을 빈 칸으로 리셋하기
+    onAdd(enteredTask);
+    setEnteredTask('');
+  }
+
   return (
     <div className="flex items-center gap-4">
-      <input type="text" className="w-64 px-2 py-1 rounded-sm bg-stone-200" />
-      <button className="text-stone-700 hover:text-stone-950">Add Task</button>
+      <input
+        type="text"
+        className="w-64 px-2 py-1 rounded-sm bg-stone-200"
+        onChange={handleChange}
+        value={enteredTask}
+      />
+      <button
+        className="text-stone-700 hover:text-stone-950"
+        onClick={handleClick}
+      >
+        Add Task
+      </button>
     </div>
   );
 }
