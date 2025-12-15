943685b72e1934a50cf3d811331ac4cf46e0b1ec
wjjhhj0914 <wjjhhj0914@gmail.com>
2025-12-15 10:37:09 +0900

    learn: 전체 데모 앱에 Context API 적용하기

--- code/10 Advanced State Management with Context useReducer/01-starting-project/src/App.jsx
+++ code/10 Advanced State Management with Context useReducer/01-starting-project/src/App.jsx
@@ -71,18 +71,16 @@ function App() {
     // state items array
     items: shoppingCart.items,
     addItemToCart: handleAddItemToCart,
+    updateItemQuantity: handleUpdateCartItemQuantity,
   };
 
   return (
     <CartContext.Provider value={ctxValue}>
-      <Header
-        cart={shoppingCart}
-        onUpdateCartItemQuantity={handleUpdateCartItemQuantity}
-      />
+      <Header />
       <Shop>
         {DUMMY_PRODUCTS.map(product => (
           <li key={product.id}>
-            <Product {...product} onAddToCart={handleAddItemToCart} />
+            <Product {...product} />
           </li>
         ))}
       </Shop>
