199d72ab6ba48c647f86bc4868d04687be8db741
wjjhhj0914 <wjjhhj0914@gmail.com>
2025-12-16 13:41:03 +0900

    learn: useEffect가 필요 없는 경우 - 다른 예시

--- code/11 Side Effects useEffect/01-starting-project/src/App.jsx
+++ code/11 Side Effects useEffect/01-starting-project/src/App.jsx
@@ -7,13 +7,17 @@ import DeleteConfirmation from './components/DeleteConfirmation.jsx';
 import logoImg from './assets/logo.png';
 import { sortPlacesByDistance } from './loc.js';
 
+const storedIds = JSON.parse(localStorage.getItem('selectedPlaces')) || [];
+const storedPlaces = storedIds.map(id =>
+  AVAILABLE_PLACES.find(place => place.id === id)
+);
+
 function App() {
   const modal = useRef();
   const selectedPlace = useRef();
   const [availablePlaces, setAvailablePlaces] = useState([]);
-  const [pickedPlaces, setPickedPlaces] = useState([]);
+  const [pickedPlaces, setPickedPlaces] = useState(storedPlaces);
 
-  // Does not return,but need two arguments
   useEffect(() => {
     navigator.geolocation.getCurrentPosition(position => {
       const sortedPlaces = sortPlacesByDistance(
@@ -43,11 +47,14 @@ function App() {
       const place = AVAILABLE_PLACES.find(place => place.id === id);
       return [place, ...prevPickedPlaces];
     });
-  }
 
-  const storedIds = JSON.parse(localStorage.getItem('selectedPlaces')) || [];
-  if (storedIds.indexOf(id) === -1) {
-    localStorage.setItem('selectedPlaces', JSON.stringify([id, ...storedIds]));
+    const storedIds = JSON.parse(localStorage.getItem('selectedPlaces')) || [];
+    if (storedIds.indexOf(id) === -1) {
+      localStorage.setItem(
+        'selectedPlaces',
+        JSON.stringify([id, ...storedIds])
+      );
+    }
   }
 
   function handleRemovePlace() {
@@ -55,6 +62,12 @@ function App() {
       prevPickedPlaces.filter(place => place.id !== selectedPlace.current)
     );
     modal.current.close();
+
+    const storedIds = JSON.parse(localStorage.getItem('selectedPlaces')) || [];
+    localStorage.setItem(
+      'selectedPlaces',
+      JSON.stringify(storedIds.filter(id => id !== selectedPlace.current))
+    );
   }
 
   return (
