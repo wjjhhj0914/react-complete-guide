a053c714605e107bc407d6cd708ff2af542f736a
wjjhhj0914 <wjjhhj0914@gmail.com>
2025-12-17 22:43:57 +0900

    learn: 브라우저 API 싱크를 위한 useEffect 활용

--- code/11 Side Effects useEffect/01-starting-project/src/components/Modal.jsx
+++ code/11 Side Effects useEffect/01-starting-project/src/components/Modal.jsx
@@ -1,10 +1,16 @@
-import { useRef } from 'react';
+import { useRef, useEffect } from 'react';
 import { createPortal } from 'react-dom';
 
 const Modal = function Modal({ open, children }) {
   const dialog = useRef();
 
-  // dialog.current.showModal();
+  useEffect(() => {
+    if (open) {
+      dialog.current.showModal();
+    } else {
+      dialog.current.close();
+    }
+  }, []);
 
   return createPortal(
     <dialog className="modal" ref={dialog} open={open}>
