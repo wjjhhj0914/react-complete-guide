9ecae60a5a33a862f85689399f3565aa13164eb2
wjjhhj0914 <wjjhhj0914@gmail.com>
2025-12-17 22:48:59 +0900

    learn: 작은 버그 수정하기

--- code/11 Side Effects useEffect/01-starting-project/src/App.jsx
+++ code/11 Side Effects useEffect/01-starting-project/src/App.jsx
@@ -72,7 +72,7 @@ function App() {
 
   return (
     <>
-      <Modal open={modalIsOpen}>
+      <Modal open={modalIsOpen} onClose={handleStopRemovePlace}>
         <DeleteConfirmation
           onCancel={handleStopRemovePlace}
           onConfirm={handleRemovePlace}
--- code/11 Side Effects useEffect/01-starting-project/src/components/Modal.jsx
+++ code/11 Side Effects useEffect/01-starting-project/src/components/Modal.jsx
@@ -1,7 +1,7 @@
 import { useRef, useEffect } from 'react';
 import { createPortal } from 'react-dom';
 
-const Modal = function Modal({ open, children }) {
+const Modal = function Modal({ open, children, onClose }) {
   const dialog = useRef();
 
   useEffect(() => {
@@ -13,7 +13,7 @@ const Modal = function Modal({ open, children }) {
   }, [open]);
 
   return createPortal(
-    <dialog className="modal" ref={dialog}>
+    <dialog className="modal" ref={dialog} onClose={onClose}>
       {children}
     </dialog>,
     document.getElementById('modal')
