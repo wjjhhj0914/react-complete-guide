efcaf85f4df9e56f307a11caf02a01c2793cb552
wjjhhj0914 <wjjhhj0914@gmail.com>
2025-12-22 22:11:22 +0900

    learn: useCallback 훅

--- code/11 Side Effects useEffect/01-starting-project/src/App.jsx
+++ code/11 Side Effects useEffect/01-starting-project/src/App.jsx
@@ -1,4 +1,4 @@
-import { useRef, useState, useEffect } from 'react';
+import { useRef, useState, useEffect, useCallback } from 'react';
 
 import Places from './components/Places.jsx';
 import { AVAILABLE_PLACES } from './data.js';
@@ -57,18 +57,18 @@ function App() {
     }
   }
 
-  function handleRemovePlace() {
+  const handleRemovePlace = useCallback(function handleRemovePlace() {
     setPickedPlaces(prevPickedPlaces =>
       prevPickedPlaces.filter(place => place.id !== selectedPlace.current)
     );
-    // setModalIsOpen(false);
+    setModalIsOpen(false);
 
     const storedIds = JSON.parse(localStorage.getItem('selectedPlaces')) || [];
     localStorage.setItem(
       'selectedPlaces',
       JSON.stringify(storedIds.filter(id => id !== selectedPlace.current))
     );
-  }
+  }, []);
 
   return (
     <>
